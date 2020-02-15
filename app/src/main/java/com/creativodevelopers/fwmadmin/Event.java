26713aac1de67c9dd00c95abea79ae8a62478ea1
Nadir <nadirkhosa@icloud.com>
2020-02-16 02:21:32 +0500

    Fetched Events in RecyclerView

---
+++ app/src/main/java/com/creativodevelopers/fwmadmin/Event.java
@@ -0,0 +1,71 @@
+package com.creativodevelopers.fwmadmin;
+
+public class Event {
+
+
+
+
+    String title,image,date,time,location,description;
+
+    public Event() {
+
+
+    }
+
+    public Event(String title, String image, String date, String time, String location, String description) {
+        this.title = title;
+        this.image = image;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/com/creativodevelopers/fwmadmin/ShowEventFragment.java
+++ app/src/main/java/com/creativodevelopers/fwmadmin/ShowEventFragment.java
@@ -2,34 +2,160 @@ package com.creativodevelopers.fwmadmin;
 
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 
 public class ShowEventFragment extends Fragment {
 
+    private View showEvent;
+    private RecyclerView showEventList;
 
+    private DatabaseReference eventRef;
 
     public ShowEventFragment() {
         // Required empty public constructor
     }
 
-    public static ShowEventFragment newInstance(String param1, String param2) {
-        ShowEventFragment fragment = new ShowEventFragment();
 
-        return fragment;
-    }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_event, container, false);
+
+        showEvent=inflater.inflate(R.layout.fragment_show_event, container, false);
+        eventRef= FirebaseDatabase.getInstance().getReference().child("Event");
+
+
+        showEventList =  showEvent.findViewById(R.id.event_list);
+        showEventList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        return showEvent;
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Event>()
+                .setQuery(eventRef, Event.class)
+                .build();
+
+
+
+        FirebaseRecyclerAdapter<Event,EventViewHolder> adapter= new FirebaseRecyclerAdapter<Event, EventViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final EventViewHolder holder, int position, @NonNull Event model) {
+
+                String eventId=getRef(position).getKey();
+
+                eventRef.child(eventId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        if(dataSnapshot.exists()){
+
+                            if(dataSnapshot.hasChild("image")){
+
+                                String eventImage=dataSnapshot.child("image").getValue().toString();
+                                String tit=dataSnapshot.child("title").getValue().toString();
+                                String des=dataSnapshot.child("description").getValue().toString();
+                                String da=dataSnapshot.child("date").getValue().toString();
+                                String ti=dataSnapshot.child("time").getValue().toString();
+                                String l=dataSnapshot.child("location").getValue().toString();
+
+                                holder.Title.setText(tit);
+                                holder.Description.setText(des);
+                                holder.Location.setText(l);
+                                holder.Date.setText(da);
+                                holder.Time.setText(ti);
+                                Picasso.get().load(eventImage).placeholder(R.drawable.eventimage).into(holder.EventImage);
+
+                            }
+                            else {
+
+                                String tit=dataSnapshot.child("title").getValue().toString();
+                                String des=dataSnapshot.child("description").getValue().toString();
+                                String da=dataSnapshot.child("date").getValue().toString();
+                                String ti=dataSnapshot.child("time").getValue().toString();
+                                String l=dataSnapshot.child("location").getValue().toString();
+
+                                holder.Title.setText(tit);
+                                holder.Description.setText(des);
+                                holder.Location.setText(l);
+                                holder.Date.setText(da);
+
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+            }
+
+            @NonNull
+            @Override
+            public EventViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+                View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.show_events_list,viewGroup,false);
+                EventViewHolder viewHolder= new EventViewHolder(view);
+
+                return viewHolder;
+
+            }
+        };
+
+        showEventList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Title, Description,Date , Time , Location;
+
+        ImageView EventImage;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            Title = itemView.findViewById(R.id.title);
+            Description = itemView.findViewById(R.id.description);
+            Date= itemView.findViewById(R.id.date);
+            Time=itemView.findViewById(R.id.time);
+            Location=itemView.findViewById(R.id.location);
+            EventImage= itemView.findViewById(R.id.Eventimage);
+
+        }
+    }
 
 }
